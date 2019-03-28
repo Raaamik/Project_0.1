faa3e15deb4244524ca4139440e47cca55928068
Raaamik <ramizaliev95@gmail.com>
2019-03-28 15:27:27 +0200

    Initial commitramik

---
+++ src/com/company/raaamik/Main.java
@@ -0,0 +1,21 @@
+package com.company.raaamik;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer a = 127;
+        Integer b = 127;
+        Integer c = 129;
+        Integer d = 129;
+
+
+
+        if(a==b)System.out.println("A==B true");
+        else System.out.println("A==B false");
+
+        if(c==d)System.out.println("A==B true");
+        else System.out.println("A==B false");
+
+    }
+}
